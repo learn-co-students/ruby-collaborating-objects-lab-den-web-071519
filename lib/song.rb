cc154af9a53440f8b6ac821ffa934d6991464a76
Your Name <tylercmasterson@gmail.com>
2019-07-22 16:51:07 -0600

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,34 +1,20 @@
-require 'pry'
+
 class Song
     attr_accessor :name, :artist
-    @@all = []
-  def initialize(name)
-    @name = name
-  end
-
-  def self.new_by_filename(filename)
-    artist, song = filename.split(" - ")
-    new_song = self.new(song)
-    new_song.artist = artist
-    new_song.save
-  end
-
-  def self.find_by_artist(artist)
-    Song.all.select do | song |
-      song.artist == artist
+  
+    def initialize(name)
+      @name = name
+    end
+  
+    def self.new_by_filename(filename)
+      artist, song = filename.split(" - ")
+      new_song = self.new(song)
+      new_song.artist_name = artist
+      new_song
+    end
+  
+    def artist_name=(name)
+      self.artist = Artist.find_or_create_by_name(name)
+      artist.add_song(self)
     end
   end
-  
-  def self.all
-    @@all
-  end
-
-  def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-  end
-  
-  def save
-    @@all << self
-    self
-  end
-end
